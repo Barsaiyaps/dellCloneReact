54659227de1010fde15f3275c52f16d10307516e
Pankaj Barsaiya <pankajbars@gmail.com>
2025-04-12 19:45:27 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import Navbar1 from './components/navbar/Navbar1'
 import Navbar2 from './components/navbar/Navbar2'
 import Footer from './components/footer/Footer'
-import Home from './pages/home'
 import { Button } from '@chakra-ui/react'
-import LoginPage from './pages/Login'
-import { Routes, Route } from 'react-router-dom'
 import AllRoutes from './routes/AllRoutes'
 
 
