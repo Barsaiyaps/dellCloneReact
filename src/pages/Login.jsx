c43cfd45422d47c2985c92511d032e7fb60a5a5b
Your Name <your.email@example.com>
2025-07-01 21:47:24 +0530

    Product Added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Signup.css";
 
 const LoginPage = () => {
-  const [formData, setFormData] = useState({ name: "", email: "", password: "" });
+  const [formData, setFormData] = useState({ "name": "", "email": "", "password": "" });
   const [message, setMessage] = useState("");
 
   const handleChange = (e) => {
@@ -11,13 +11,14 @@ const LoginPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(JSON.stringify(formData))
     try {
       const response = await fetch("https://dellcloncebackend-1.onrender.com/dell/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
-      const data = await response.json();
+      const data = await response;
       setMessage(data.message || "Registration successful!");
     } catch (error) {
       setMessage(error.message || "Something went wrong!");
@@ -28,9 +29,9 @@ const LoginPage = () => {
     <div className="signup-container">
       <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
-        <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
-        <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
+        <input type="text" name="name" placeholder="Name" value={formData.name.trim()} onChange={handleChange} required />
+        <input type="email" name="email" placeholder="Email" value={formData.email.trim()} onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Password" value={formData.password.trim()} onChange={handleChange} required />
         <button type="submit">Register</button>
       </form>
       {message && <p className="message">{message}</p>}
